1da26ea550256c63c3921bb8903e3d3facb26fc0
Alan O' Reilly <oreillyalan88@gmail.com>
2016-12-30 12:45:38 +0000

    Add User Following
    Former-commit-id: 85fdcf4df5e84073e246b0a84c21b2c0dd8adb52

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,17 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
   resources :movies do
     collection do
       get 'search'
     end
+    
     resources :reviews do
       member do
         put "like", to: "reviews#upvote"
@@ -27,5 +33,6 @@ Rails.application.routes.draw do
   end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy] 
+  resources :reviews,          only: [:create, :destroy] 
+  resources :relationships,       only: [:create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230033724) do
+ActiveRecord::Schema.define(version: 20161230115815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 20161230033724) do
     t.string   "movie_img_content_type"
     t.integer  "movie_img_file_size"
     t.datetime "movie_img_updated_at"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true, using: :btree
+    t.index ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
   end
 
   create_table "reviews", force: :cascade do |t|
