a1251e9c5a4a7d06a7cd05c70b3899e0065553b7
Alan O' Reilly <oreillyalan88@gmail.com>
2017-01-03 02:54:08 +0000

    Added accont activation for deployment
    Former-commit-id: a5103f8e7905cdc8c70881613ddd16205aa896cf

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,3 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "password",
-             password_confirmation: "password",
-             admin: true,
-             activated: true,
-             activated_at: Time.zone.now)
-             
-
-User.create!(name:  "Example User2",
-             email: "example2@railstutorial.org",
-             password:              "password",
-             password_confirmation: "password",
-             admin: true,
-             activated: true,
-             activated_at: Time.zone.now)
-
 Genre.create!(name:  "Sci - Fi")
 Genre.create!(name:  "Thriller")
 Genre.create!(name:  "Drama")
