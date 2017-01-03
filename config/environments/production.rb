c77c1fa701a01e363d1621dd69ca40044765e76f
Alan O' Reilly <oreillyalan88@gmail.com>
2017-01-03 02:48:05 +0000

    Added accont activation for deployment
    Former-commit-id: c01206c0104239cd4835dc2707f52b145823cd0e

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
   config.force_ssl = true
-  
+  config.public_file_server.enabled = true
   
   
   
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,20 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+             password:              "password",
+             password_confirmation: "password",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+             
+
+User.create!(name:  "Example User2",
+             email: "example2@railstutorial.org",
+             password:              "password",
+             password_confirmation: "password",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
 
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end
-
-
-# Following relationships
-users = User.all
-user  = users.first
-following = users[2..50]
-followers = users[3..40]
-following.each { |followed| user.follow(followed) }
-followers.each { |follower| follower.follow(user) }
+Genre.create!(name:  "Sci - Fi")
+Genre.create!(name:  "Thriller")
+Genre.create!(name:  "Drama")
