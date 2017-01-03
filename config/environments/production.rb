9391d56849d018813d32a33f22d6ca054b319d9e
Alan O' Reilly <oreillyalan88@gmail.com>
2017-01-03 02:36:11 +0000

    Added accont activation for deployment
    Former-commit-id: f03d017e549ac8ca4a84751d3e5baa83c4102172

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,9 +86,9 @@ Rails.application.configure do
   
   
   
-     config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
-    host = '<your heroku app>.herokuapp.com'
+    host = 'https://limitless-earth-16641.herokuapp.com'
     config.action_mailer.default_url_options = { host: host }
     ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
