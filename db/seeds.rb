870fc7dc319886f3cbd89d9edb79462fcf63001f
Alan O' Reilly <oreillyalan88@gmail.com>
2017-01-03 04:01:01 +0000

    Clearing for deploy
    Former-commit-id: 27a8b577f7ad89c875fc8e11c08574fa8ce5ab14

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
+
+
 Genre.create!(name:  "Sci-Fi")
 Genre.create!(name:  "Drama")
 Genre.create!(name:  "Thriller")
